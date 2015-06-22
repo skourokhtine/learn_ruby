a53de190b1f13224bc9d4f4deeefc06abbaf6723
Serguei Kourokhtine <skourokhtine@gmail.com>
2015-06-22 19:03:31 -0400

    Simon is in progress

---
+++ 03_simon_says/simon.rb
@@ -0,0 +1,28 @@
+def echo(word)
+"#{word}"
+end
+
+echo("hello")
+echo("bye")
+
+
+def shout(word)
+"#{word.upcase}"
+end
+
+shout("hello")
+
+
+def shoult(word)
+	"#{word.upcase}"
+end
+
+shoult("hello word")
+
+def repeat(word)
+ 2.times "#{word}"
+end
+
+repeat("hello")
+
+
