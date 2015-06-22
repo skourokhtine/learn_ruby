1af853e77de752d91cf287bfd66423241dd0c15a
Serguei Kourokhtine <skourokhtine@gmail.com>
2015-06-22 15:44:07 -0400

    Calculator test accomplished

---
+++ 02_calculator/calculator.rb
@@ -0,0 +1,22 @@
+def subtract(x, y)
+x - y
+end
+
+
+def add(x, y)
+	x + y
+end
+
+def sum(array)
+	total = 0
+	array.each do |n| 
+		total += n
+	end
+	total
+end	
+
+def multiply(x, y)
+	x * y
+end
+
+
